4e953b864ac456c9ef08e40cfb69cf54cccf0c3b
Hyunjin Kim <hyun.jin.kim.1112@gmail.com>
2026-04-20 15:28:54 -0400

    Initial Commit

---
+++ Makerspace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Makerspace
+//
+//  Created by Hyunjin Kim on 20/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Makerspace/MakerspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakerspaceApp.swift
+//  Makerspace
+//
+//  Created by Hyunjin Kim on 20/4/26.
+//
+
+import SwiftUI
+
+@main
+struct MakerspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
